9b8b48ef36f69ad0c4abba535e5b3c3c619f68cb
igak0913 <igor.o.akimov@netcracker.com>
2017-11-27 14:37:16 +0200

    Added isChildrenProportional flag. Added fixedSize flag. Added onResize event handler.

--- layoutGenerator.js
+++ layoutGenerator.js
@@ -1,77 +1,276 @@
 
 let dashboardConfig = {
-	id: "1962e678-c88a-4611-9b2b-c10c9e7ee0dd",
-    name: "Dashboard: User with DMP",
+    "id": "1962e678-c88a-4611-9b2b-c10c9e7ee0dd",
+    "name": "Dashboard: User with DMP",
 	containers: [ 
 		{
-			type: "horizontal",
+			type: "HORIZONTAL",
 			minHeight: 30,
-			widgets: [
-				"<img src='./images/test-logo.png' />",
-				"<img src='./images/test-logo.png' />"
-			]
-		}, 
-		{
-			type: "horizontal",
-			minHeight: 40,
-			containers: [ 
+			containers: [
 				{
-					type: "vertical",
+					type: "VERTICAL",
 					width: 33,
 					widgets: [
-						"<img src='./images/test-logo-small.png' />",
-						"<img src='./images/test-logo-small.png' />"
-					]
-				}, 
-				{
-					type: "vertical",
-					width: 33,
-					containers: [ 
-						{
-							type: "horizontal",
-							minHeight: 35,
-							widgets: ["w3"]
-						}, 
-						{
-							type: "horizontal",
-							minHeight: 65,
-							containers: [ 
-								{
-									type: "horizontal",
-									minHeight: 35,
-									widgets: ["w5"]
-								}, 
-								{
-									type: "horizontal",
-									minHeight: 65,
-									containers: [ 
-										{
-											type: "horizontal",
-											minHeight: 35,
-											widgets: ["<img src='./images/test-logo.png' />"]
-										}, 
-										{
-											type: "horizontal",
-											minHeight: 65,
-											widgets: ["w8"],	
-										}					
-									]									
-								}					
-							]	
-						}
+						"44a28105-9061-46d2-b532-2ed61f03901e"
 					]
 				},
 				{
-					type: "vertical",
-					width: 33
-				}, 
+					type: "VERTICAL",
+					width: 33,
+					widgets: [
+						"d390616c-4894-422e-9fc7-d1c985f477d4"
+					]
+				},
+				{
+					type: "VERTICAL",
+					width: 33,
+					widgets: [
+						"d2d7a411-4c89-4bf9-b22a-91ce27f0d1e9"
+					]
+				}
 			]
+		}, 
+		{
+			type: "HORIZONTAL",
+			minHeight: 30,
+			widgets: [
+				"8f342f5b-d979-4b4a-a2b8-b6afdd8160e5"
+		    ]
+		},
+		{
+			type: "HORIZONTAL",
+			minHeight: 10,
+			widgets: [
+				"022f3350-a7cc-4a51-9bd6-d9e8f2334a91"
+		    ]
+		},
+		{
+			type: "HORIZONTAL",
+			minHeight: 10,
+			widgets: [
+				"d422fe17-cd99-4b9e-b760-9f67ac413a8"
+		    ]
+		},
+		{
+			type: "HORIZONTAL",
+			minHeight: 30,
+			containers: [
+			    {
+					type: "VERTICAL",
+					width: 50,
+					widgets: [
+						"dc210bdc-cace-41ea-8c2c-35e8c6b18e5c"
+					]
+				},
+				{
+					type: "VERTICAL",
+					width: 50,
+					widgets: [
+						"10ca0332-b9ee-4bf7-9af0-eb99ab546a87"
+					]
+				}
+			]
+			
 		}
-]		
+	]		
 };
 
-const HORIZONTAL_CONTAINER_TYPE = "horizontal";
-const VERTICAL_CONTAINER_TYPE = "vertical";
+let dashboardConfig2 = {
+    "id": "20083ba2-5129-43f8-8ecc-fe9eea4d09b9",
+    "name": "Dashboard: User without DMP",
+	containers: [ 
+		{
+			type: "HORIZONTAL",
+			minHeight: 30,
+			containers: [
+				{
+					type: "VERTICAL",
+					width: 33,
+					widgets: [
+						"44a28105-9061-46d2-b532-2ed61f03901e"
+					]
+				},
+				{
+					type: "VERTICAL",
+					width: 33,
+					widgets: [
+						"d390616c-4894-422e-9fc7-d1c985f477d4"
+					]
+				},
+				{
+					type: "VERTICAL",
+					width: 33,
+					widgets: [
+						"d2d7a411-4c89-4bf9-b22a-91ce27f0d1e9"
+					]
+				}
+			]
+		}, 
+		{
+			type: "HORIZONTAL",
+			minHeight: 30,
+			widgets: [
+				"8f342f5b-d979-4b4a-a2b8-b6afdd8160e5"
+		    ]
+		},
+		{
+			type: "HORIZONTAL",
+			minHeight: 10,
+			widgets: [
+				"022f3350-a7cc-4a51-9bd6-d9e8f2334a91"
+		    ]
+		}
+	]		
+};
+
+let dashboardConfig3 = {
+    "id": "fec8b8fb-40ef-4fd9-b260-6faa5531505e",
+    "name": "Dashboard: Manager with DMP",
+	containers: [ 
+		{
+			type: "HORIZONTAL",
+			minHeight: 30,
+			containers: [
+				{
+					type: "VERTICAL",
+					width: 33,
+					widgets: [
+						"44a28105-9061-46d2-b532-2ed61f03901e"
+					]
+				},
+				{
+					type: "VERTICAL",
+					width: 33,
+					widgets: [
+						"d390616c-4894-422e-9fc7-d1c985f477d4"
+					]
+				},
+				{
+					type: "VERTICAL",
+					width: 33,
+					widgets: [
+						"d2d7a411-4c89-4bf9-b22a-91ce27f0d1e9"
+					]
+				}
+			]
+		}, 
+		{
+			type: "HORIZONTAL",
+			minHeight: 30,
+			widgets: [
+				"8f342f5b-d979-4b4a-a2b8-b6afdd8160e5"
+		    ]
+		},
+		{
+			type: "HORIZONTAL",
+			minHeight: 10,
+			widgets: [
+				"022f3350-a7cc-4a51-9bd6-d9e8f2334a91"
+		    ]
+		},
+			{
+			type: "HORIZONTAL",
+			minHeight: 30,
+			containers: [
+			    {
+					type: "VERTICAL",
+					width: 50,
+					widgets: [
+						"f7de7e1e-d91a-4efc-8eb6-e39d9367ef62"
+					]
+				},
+				{
+					type: "VERTICAL",
+					width: 50,
+					widgets: [
+						"266f1691-3e6b-4060-ba4d-51d308e4c7cc"
+					]
+				}
+			]
+			
+		},
+		{
+			type: "HORIZONTAL",
+			minHeight: 10,
+			widgets: [
+				"d422fe17-cd99-4b9e-b760-9f67ac413a85"
+		    ]
+		},
+		{
+			type: "HORIZONTAL",
+			minHeight: 30,
+			containers: [
+			    {
+					type: "VERTICAL",
+					width: 50,
+					widgets: [
+						"dc210bdc-cace-41ea-8c2c-35e8c6b18e5c"
+					]
+				},
+				{
+					type: "VERTICAL",
+					width: 50,
+					widgets: [
+						"10ca0332-b9ee-4bf7-9af0-eb99ab546a87"
+					]
+				}
+			]
+			
+		}
+	]		
+};
+
+let dashboardConfig4 = {
+    "id": "fd82d6c6-7abb-450d-93cc-d24b0ff9306e",
+    "name": "Dashboard: Manager without DMP",
+	containers: [ 
+		{
+			type: "HORIZONTAL",
+			minHeight: 30,
+			containers: [
+				{
+					type: "VERTICAL",
+					width: 40,
+					widgets: [
+						"44a28105-9061-46d2-b532-2ed61f03901e"
+					]
+				},
+				{
+					type: "VERTICAL",
+					width: 40,
+					widgets: [
+						"d390616c-4894-422e-9fc7-d1c985f477d4"
+					]
+				},
+				{
+					type: "VERTICAL",
+					width: 20,
+					widgets: [
+						"d2d7a411-4c89-4bf9-b22a-91ce27f0d1e9"
+					]
+				}
+			]
+		}, 
+		{
+			type: "HORIZONTAL",
+			minHeight: 30,
+			widgets: [
+				"8f342f5b-d979-4b4a-a2b8-b6afdd8160e5"
+		    ]
+		},
+		{
+			type: "HORIZONTAL",
+			minHeight: 10,
+			widgets: [
+				"022f3350-a7cc-4a51-9bd6-d9e8f2334a91"
+		    ]
+		}
+	]		
+};
+
+
+const HORIZONTAL_CONTAINER_TYPE = "HORIZONTAL";
+const VERTICAL_CONTAINER_TYPE = "VERTICAL";
 const HORIZONTAL_CONTAINER_CSS_CLASS = "layout-container-hirizontal";
 const VERTICAL_CONTAINER_CSS_CLASS = "layout-container-vertical";
 const DISPLAY_NONE = "display: none";
@@ -125,18 +324,25 @@ function getContainerOpenTag(containerConfig, dashboardLocalParams) {
 }
 
 /**
-* Generate HTML 'open-tag' of the specified HORIZONTAL container
-**/
+ * Generate HTML 'open-tag' of the specified HORIZONTAL container
+ **/
 function getHorizontalOpenTag(containerConfig, dashboardLocalParams) {
-	//Calculate minHeight
-	let minHeight = containerConfig.minHeight;
-	if (minHeight == undefined || minHeight < 0) {
-		throw new Error("minHeight is not specified for HORIZONTAL container (containerConfig = " + JSON.stringify(containerConfig)+ ")");
-	}
-	let minHeightParam = minHeightOrDefault(containerConfig.minHeight * dashboardLocalParams.dashboardHeight / 100, 0);
-return '<div style="' 
-	+ minHeightParam + ';' 
-	+ '" class="' + HORIZONTAL_CONTAINER_CSS_CLASS + '">';
+    //Calculate minHeight
+    let minHeight = containerConfig.minHeight;
+    if (minHeight == undefined || minHeight < 0) {
+        throw new Error("minHeight is not specified for HORIZONTAL container (containerConfig = " + JSON.stringify(containerConfig) + ")");
+    }
+    let minHeightAbsolute = convertPercentHeightToAbsolute(minHeight, dashboardLocalParams);
+    let minHeightParam = this.minHeightOrDefault(minHeightAbsolute, 0);
+    let isFixedSize = containerConfig.fixedSize;
+    let cssClasses = HORIZONTAL_CONTAINER_CSS_CLASS;
+    if (isFixedSize) {
+        cssClasses += " fixed";
+    }
+    return '<div '
+        + 'minHeightPercent=' + minHeight
+        + ' style="' + minHeightParam + ';'
+        + '" class="' + cssClasses + '">';
 }
 
 /**
@@ -201,51 +407,71 @@ function getContainerContent(containerConfig, dashboardLocalParams) {
 
 
 /**
-* Retrive inner HTML with widgets for specified container config
-**/
+ * Retrive inner HTML with widgets for specified container config
+ **/
 function getHtmlForWidgets(containerConfig, dashboardLocalParams) {
-	//TODO: Implement
-	let widgetsHTML = "";
-	if (containerConfig.widgets) {
-		containerConfig.widgets.forEach((w) => {
-			switch(containerConfig.type) {
-				case HORIZONTAL_CONTAINER_TYPE:
-					if (dashboardLocalParams.isMobile) {
-						widgetsHTML += wrapWidgetInHorizontalBlock(w);
-					} else {
-						widgetsHTML += wrapWidgetInVerticalBlock(w);
-					}
-					break;
-				case VERTICAL_CONTAINER_TYPE:
-					widgetsHTML += wrapWidgetInHorizontalBlock(w);
-					break;
-				default:
-					throw new Error('Unexpected type "' + containerConfig.type + '" of widget parent container  (containerConfig = "' + JSON.stringify(containerConfig) + '")...');
-			};		
-		});
-	}
-	return widgetsHTML;
+    let widgetsHTML = "";
+    if (containerConfig.widgets) {
+        let isChildrenProportional = containerConfig.isChildrenProportional;
+        let widgetsCount = containerConfig.widgets.length;
+        containerConfig.widgets.forEach((w) => {
+            switch (containerConfig.type) {
+                case HORIZONTAL_CONTAINER_TYPE:
+                    if (dashboardLocalParams.isMobile) {
+                        widgetsHTML += this.wrapWidgetInHorizontalBlock(w, widgetsCount, isChildrenProportional);
+                    } else {
+                        widgetsHTML += this.wrapWidgetInVerticalBlock(w, widgetsCount, isChildrenProportional);
+                    }
+                    break;
+                case VERTICAL_CONTAINER_TYPE:
+                    widgetsHTML += this.wrapWidgetInHorizontalBlock(w, widgetsCount, isChildrenProportional);
+                    break;
+                default:
+                    throw new Error('Unexpected type "' + containerConfig.type + '" of widget parent container  (containerConfig = "' + JSON.stringify(containerConfig) + '")...');
+            }
+        });
+    }
+    return widgetsHTML;
 }
 
 /**
-* Draw widget and wrap it with horizontal block
-**/
-function wrapWidgetInHorizontalBlock(widgetConfig) {
-	return wrapWidget(widgetConfig, WIDGET_HORIZONTAL_WRAPPER_CSS_CLASS);
+ * Draw widget and wrap it with horizontal block
+ **/
+function wrapWidgetInHorizontalBlock(widgetConfig, siblingsCount, isChildrenProportional) {
+    let customStyle = "";
+    //Widgets(wrapped with horizontal-wrapper) in VERTICAL block are proportional by default.
+    customStyle = "height: " + ( 100 / siblingsCount ) + "%";
+    //Widgets(wrapped with vertical-wrapper) in HORIZONTAL block are proportional by default.
+    if (!isChildrenProportional && isChildrenProportional != undefined) {
+        customStyle = "";
+    }
+    return this.wrapWidget(widgetConfig, WIDGET_HORIZONTAL_WRAPPER_CSS_CLASS, customStyle);
 }
 
 /**
-* Draw widget and wrap it with horizontal block
-**/
-function wrapWidgetInVerticalBlock(widgetConfig) {
-	return wrapWidget(widgetConfig, WIDGET_VERTICAL_WRAPPER_CSS_CLASS);
+ * Draw widget and wrap it with horizontal block
+ **/
+function wrapWidgetInVerticalBlock(widgetConfig, siblingsCount, isChildrenProportional) {
+    let customStyle = "width: " + ( 100 / siblingsCount ) + "%";
+    //Widgets(wrapped with vertical-wrapper) in HORIZONTAL block are proportional by default.
+    if (!isChildrenProportional && isChildrenProportional != undefined) {
+        customStyle = "";
+    }
+    return this.wrapWidget(widgetConfig, WIDGET_VERTICAL_WRAPPER_CSS_CLASS, customStyle);
 }
 
 /**
-* Draw widget and wrap it with generic div
-**/
-function wrapWidget(widgetConfig, cssClasses) {
-	return '<div class="' + cssClasses + '">' + getWidgetHTML(widgetConfig) + '</div>';
+ * Draw widget and wrap it with generic div
+ **/
+function wrapWidget(widgetConfig, cssClasses, customStyle) {
+    if (customStyle != undefined && customStyle.length > 0) {
+        customStyle = 'style="' + customStyle + '"';
+    } else {
+        customStyle = "";
+    }
+    return '<div class="' + cssClasses + '" ' + customStyle + ' data-widget-id="' + widgetConfig + '">' 
+		+ getWidgetHTML(widgetConfig) 
+		+ '</div>';
 }
 
 /**
@@ -263,16 +489,66 @@ function getContainerCloseTag(containerConfig) {
 	return "</div>";
 }
 
+/**
+ * Convert height in percents (of screens height) to absolute value.
+ */
+function convertPercentHeightToAbsolute(minHeightPercent, dashboardLocalParams) {
+    return minHeightPercent * dashboardLocalParams.dashboardHeight / 100;
+}
+
+/**
+ * Retrieve height of the screen
+ * @return {number} height of the screen
+ */
+function getScreenHeight() {
+    let body = document.body;
+    let html = document.documentElement;
+    return Math.max(body.offsetHeight, html.clientHeight, html.offsetHeight);
+}
+
+/**
+ * Calculate dasboard params specific for user device (width, height, isMobile etc.)
+ */
+function getDashboardLocalParams(dashboardDiv) {
+    return {
+        dashboardWidth: dashboardDiv.clientWidth,
+        dashboardHeight: getScreenHeight(),
+        isMobile: false
+    };
+}
+
+/**
+ * On Window resize event handler. Will change min-height of all horizontal containers.
+ * Because only horizontal containers have such parameter.
+ * Also there is no need to change width parameters because they already have values in percents.
+ *
+ * @param event
+ */
+function onResize(event) {
+    window['console']['log']("window:resize");
+    let d = document.querySelector("#dashboard");
+    let dashboardLocalParams = this.getDashboardLocalParams(d);
+    let els = document.getElementsByClassName(HORIZONTAL_CONTAINER_CSS_CLASS);
+    Array.prototype.forEach.call(els, function (el) {
+        let minHeightAbs = convertPercentHeightToAbsolute(
+            el.attributes["minHeightPercent"].value,
+            dashboardLocalParams
+        );
+        el.style["min-height"] = minHeightAbs + "px";
+    });
+}
 
 document.addEventListener('DOMContentLoaded', function(){ 
     let d = document.querySelector("#dashboard");
-	let dashboardLocalParams = {
-		dashboardWidth: d.clientWidth,
-	    dashboardHeight: d.clientHeight,
-		isMobile: false
-	};
+	let dashboardLocalParams = getDashboardLocalParams(d);
+    //Set 'is-mobile' class to dasboard if needed
+    if (dashboardLocalParams.isMobile) {
+        d.className += " " + "is-mobile";
+    }
 	console.log(dashboardLocalParams);
-	let layoutHTML = getContainerContent(dashboardConfig, dashboardLocalParams);
+	let layoutHTML = getContainerContent(dashboardConfig4, dashboardLocalParams);
 	//Set leyout to dashboard
 	d.innerHTML = layoutHTML;
 });
+
+window.addEventListener("resize", onResize);
